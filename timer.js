983a21d5457bae325015a3760d7ca936179e818b
dszopa <szopanator@gmail.com>
2017-03-24 21:09:17 -0500

    Clean up code & Finish implementation - Clean up javascript code, organize functions & add documentation - Implement pause, reset, & clear

--- timer.js
+++ timer.js
@@ -1,4 +1,28 @@
 /**
+ * Returns a json list with the current hours, minutes, and seconds of the timer.
+ */
+function getClockTime() {
+  var timeFormElements = document.forms['timeForm'].getElementsByTagName('input');
+  return {
+    'hours': parseInt(timeFormElements.hours.value),
+    'minutes': parseInt(timeFormElements.minutes.value),
+    'seconds': parseInt(timeFormElements.seconds.value)
+  }
+}
+
+/**
+ * Sets the clock time to the given input.
+ * @param {Number} hours 
+ * @param {Number} minutes 
+ * @param {Number} seconds 
+ */
+function setClockTime(hours, minutes, seconds) {
+  document.getElementById('hours').value = hours;
+  document.getElementById('minutes').value = minutes;
+  document.getElementById('seconds').value = seconds;
+}
+
+/**
  * A function that gets the amount of time between the date end time
  * and the current time.
  * @param {Date} endtime - The end time in a Date format.
@@ -17,54 +41,18 @@ function getTimeRemaining(endtime) {
 }
 
 /**
- * A function that "zero's" the clock. This involves setting all inputs
- * to 00.
- */
-function zeroClock() {
-  var clock = document.getElementById("clockdiv");
-  clock.querySelector('#hours').value = '00';
-  clock.querySelector('#minutes').value = '00';
-  clock.querySelector('#seconds').value = '00';
-}
-
-/**
- * Ends the specified interval, updates the button to "Start Timer",
- * pauses color changing, and resets the clock.
- * @param {setInterval} runningInterval - The ongoing interval to stop.
- */
-function stopClockAndUpdate(runningInterval) {
-  clearInterval(runningInterval);
-  document.getElementById("startBtn").innerHTML = "Start Timer";
-  document.getElementById('lapNumber').innerHTML = 0;
-  document.body.setAttribute("class", "colorchange pause-colorchange");
-  zeroClock();
-}
-
-/**
- * Returns a json list with the current hours, minutes, and seconds of the timer.
- */
-function getTimeFormValues() {
-  var timeFormElements = document.forms["timeForm"].getElementsByTagName("input");
-  return {
-    'hours': parseInt(timeFormElements.hours.value),
-    'minutes': parseInt(timeFormElements.minutes.value),
-    'seconds': parseInt(timeFormElements.seconds.value)
-  }
-}
-
-/**
  * Returns true if there is time remaining on the clock.
- * @param {TimeFormValue} timeFormValues - A json object of timeform values iwth hours, minutes, and seconds
+ * @param {JSON} timeFormValues - A json object of timeform values with hours, minutes, and seconds
  */
-function isTimeRemaining(timeFormValues) {
-  return !(timeFormValues.hours === 0 && timeFormValues.minutes === 0 && timeFormValues.seconds === 0);
+function isTimeRemaining(currentClockTime) {
+  return !((currentClockTime.hours === 0) && (currentClockTime.minutes === 0) && (currentClockTime.seconds === 0));
 }
 
 /**
  * Takes an input field and validates that it has surrounding 0's
  * @param {Element} input - The corresponding input box to be validated
  */
-function validateInput(input) {
+function validateClockInput(input) {
   if (input.value.length === 1) {
     input.value = '0' + input.value;
   } else if (input.value.length === 0) {
@@ -72,19 +60,64 @@ function validateInput(input) {
   }
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
+/**
+ * Convert clock time to seconds
+ * @param {JSON} clockTime - Clock time in JSON
+ */
+function clockTimeToSeconds(clockTime) {
+  return (clockTime.hours * 60 * 60 * 1000) + (clockTime.minutes * 60 * 1000) + ((clockTime.seconds) * 1000);
+}
+
+/**
+ * A function that 'zero's' the clock. This involves setting all inputs
+ * to 00.
+ */
+function zeroClock() {
+  var clock = document.getElementById('clockdiv');
+  clock.querySelector('#hours').value = '00';
+  clock.querySelector('#minutes').value = '00';
+  clock.querySelector('#seconds').value = '00';
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
   zeroClock();
 
   // Define timer variables
-  var deadline = new Date();
-  var totalTime = 0;
+  var startbtn = document.getElementById('startBtn');
+  var clockInterval = null;
+  var clockEndTime = new Date();
+  var clockStartValues = {
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  };
 
-  var startTimer = null;
-  var startbtn = document.getElementById("startBtn");
+  /**
+   * This function is within the onPageLoad event listener because it needs access to clockInterval
+   * @param {JSON} options 
+   */
+  function updateClock(options) {
+    var timeRemaining = getTimeRemaining(clockEndTime);
+    setClockTime(('0' + timeRemaining.hours).slice(-2), ('0' + timeRemaining.minutes).slice(-2), ('0' + timeRemaining.seconds).slice(-2));
 
-  var hoursInput = document.getElementById('hours');
-  var minutesInput = document.getElementById('minutes');
-  var secondsInput = document.getElementById('seconds');
+    if (timeRemaining.total <= 0) {
+      if (options.lap) {
+        var lapNum = document.getElementById('lapNumber');
+        lapNum.innerHTML = parseInt(lapNum.innerHTML) + 1;
+      }
+      if (options.repeat) {
+        // Add an extra second to start from original value.
+        clockEndTime = new Date(Date.parse(new Date()) + clockStartValues + (1000));
+      } else {
+        clearInterval(clockInterval);
+        startbtn.innerHTML = 'Start';
+        document.body.setAttribute('class', 'colorchange pause-colorchange');
+      }
+    }
+  }
+
+
+  /////////////////////////////// Action Handlers ////////////////////////////////
 
   // Add actions for onfocus and onblur to each timerInput
   var timerInputs = document.getElementsByClassName('timerInput');
@@ -97,60 +130,93 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     // If shorter than 2, add 0's
     input.onblur = function () {
-      validateInput(this);
-    }
-  }
-
-  // This function is within the onPageLoad event listener because it needs access to startTimer
-  function updateClock(endTime, options) {
-    var timeRemaining = getTimeRemaining(endTime);
-    hoursInput.value = ('0' + timeRemaining.hours).slice(-2);
-    minutesInput.value = ('0' + timeRemaining.minutes).slice(-2);
-    secondsInput.value = ('0' + timeRemaining.seconds).slice(-2);
-
-    if (timeRemaining.total <= 0) {
-      if (options.repeat) {
-        if (options.lap) {
-          var lapNum = document.getElementById('lapNumber');
-          lapNum.innerHTML = parseInt(lapNum.innerHTML) + 1;
-        }
-        // Add an extra second to start from original value.
-        deadline = new Date(Date.parse(new Date()) + totalTime + (1000));
-      } else {
-        stopClockAndUpdate(startTimer);
-      }
+      validateClockInput(this);
     }
   }
 
   startbtn.onclick = function () {
-    var timeFormValues = getTimeFormValues();
-    if (startbtn.innerHTML === "Start Timer" && isTimeRemaining(timeFormValues)) {
 
-      document.body.setAttribute("class", "colorchange");
-      startbtn.innerHTML = "Stop Timer";
+    var currentClockTime = getClockTime();
 
-      totalTime = (timeFormValues.hours * 60 * 60 * 1000) + (timeFormValues.minutes * 60 * 1000) +
-        ((timeFormValues.seconds) * 1000);
-      deadline = new Date(Date.parse(new Date()) + totalTime);
+    if (startbtn.innerHTML === 'Start' && isTimeRemaining(currentClockTime)) {
 
-      startTimer = setInterval(
+      document.body.setAttribute('class', 'colorchange');
+      startbtn.innerHTML = 'Pause';
+
+      clockStartValues = currentClockTime;
+      clockEndTime = new Date(Date.parse(new Date()) + clockTimeToSeconds(clockStartValues));
+
+      clockInterval = setInterval(
         function () {
           var options = {
-            "repeat": document.getElementById("repeat-chk-box").checked,
-            "lap": document.getElementById("lap-chk-box").checked
+            'repeat': document.getElementById('repeat-chk-box').checked,
+            'lap': document.getElementById('lap-chk-box').checked
           };
-          updateClock(deadline, options);
+          updateClock(options);
         }, 1000);
-    } else {
-      stopClockAndUpdate(startTimer)
+
+    } else if (startbtn.innerHTML === 'Pause') {
+
+      clearInterval(clockInterval);
+      document.getElementById('startBtn').innerHTML = 'Unpause';
+      document.body.setAttribute('class', 'colorchange pause-colorchange');
+
+    } else if (startbtn.innerHTML === 'Unpause') {
+
+      document.body.setAttribute('class', 'colorchange');
+      startbtn.innerHTML = 'Pause';
+
+      var timeOnClock = clockTimeToSeconds(currentClockTime);
+
+      if (timeOnClock === 0) {
+        timeOnClock = clockTimeToSeconds(clockStartValues) + 1000;
+      }
+
+      clockEndTime = new Date(Date.parse(new Date()) + timeOnClock);
+
+      clockInterval = setInterval(function () {
+        var options = {
+          'repeat': document.getElementById('repeat-chk-box').checked,
+          'lap': document.getElementById('lap-chk-box').checked
+        };
+        updateClock(options);
+      }, 1000);
+
     }
   }
 
+  var resetbtn = document.getElementById('resetBtn');
+  resetbtn.onclick = function () {
+    clearInterval(clockInterval);
+    startbtn.innerHTML = 'Start';
+    document.getElementById('lapNumber').innerHTML = 0;
+    setClockTime(clockStartValues.hours, clockStartValues.minutes, clockStartValues.seconds);
+    document.body.setAttribute('class', 'colorchange pause-colorchange');
+
+    for (var i = 0; i < timerInputs.length; i++) {
+      validateClockInput(timerInputs.item(i));
+    }
+  }
+
+  var clearbtn = document.getElementById('clearBtn');
+  clearbtn.onclick = function () {
+    clearInterval(clockInterval);
+    zeroClock();
+    startbtn.innerHTML = 'Start';
+    document.getElementById('lapNumber').innerHTML = 0;
+    document.getElementById('lapDiv').setAttribute('class', 'hidden');
+    document.getElementById('repeat-chk-box').checked = false;
+    document.getElementById('lap-chk-box').checked = false;
+    document.body.setAttribute('class', 'colorchange pause-colorchange');
+  }
+
   // Toggle display of lapDiv
-  document.getElementById('lap-chk-box').onclick = function () {
+  var lapDiv = document.getElementById('lap-chk-box');
+  lapDiv.onclick = function () {
     if (this.checked) {
       document.getElementById('lapDiv').setAttribute('class', '');
     } else {
+      document.getElementById('lapNumber').innerHTML = 0;
       document.getElementById('lapDiv').setAttribute('class', 'hidden');
     }
   }
